5c72dfddd95299b7444e8f3f6d0af9966fcfc97b
Swaty715 <116383234+Swaty715@users.noreply.github.com>
2022-11-06 22:44:01 +0500

    AfriDii005.py
    pkg update && pkg upgrade
    pkg install python
    pip install mechanize requests bs4
    pkg install git
    git clone https://github.com/Swaty715/AfriDii005.git
    cd AfriDii005
    python AfriDii005.py

---
+++ AfriDii005.py
@@ -0,0 +1,106 @@
+# Decompile by Mardis (Tools By Kapten-Kaizo)
+# Time Succes decompile : 2022-06-02 12:49:59.606018
+import os
+import sys
+import time
+import requests
+import random
+import platform
+import base64
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+
+def runtxt(z):
+    for e in z + "\n":
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+def notice():
+	print("")
+	runtxt('    Wating For Anousment And Voting In Group Stay Happy')
+	print("")
+	runtxt("\n        Admin :- Marwan")
+	print("")
+	runtxt("\033[1;32m        FB Account :- YAsir AfriDii")
+	print("")
+	runtxt("\033[1;33m        Team :- CHAND + SONA + LOVER")
+	print("")
+	runtxt("\033[1;33m            🌹🌹🌹🌹🌹🌹🌹🌹🌹🌹")
+	print("")
+	runtxt("\033[1;36m          Pro Hackers Or Programers")
+	input()
+logo="""
+
+{__       {__      {_       {_______    {__        {__      {_       {___     {__
+{_ {__   {___     {_ __     {__    {__  {__        {__     {_ __     {_ {__   {__
+{__ {__ { {__    {_  {__    {__    {__  {__   {_   {__    {_  {__    {__ {__  {__
+{__  {__  {__   {__   {__   {_ {__      {__  {__   {__   {__   {__   {__  {__ {__
+{__   {_  {__  {______ {__  {__  {__    {__ {_ {__ {__  {______ {__  {__   {_ {__
+{__       {__ {__       {__ {__    {__  {_ {_    {____ {__       {__ {__    {_ __
+{__       {__{__         {__{__      {__{__        {__{__         {__{__      {__
+                                                                                 
+
+os.system('clear')
+print(logo)
+print("[🔥]•••••••••••••••••{C + S}•••••••••••••••••[🔥]")
+print("")
+print ("\033[1;32m     Welcome 🤪 To Marwan And Win 500 Rupees And  😊 Accounts")
+print("")
+print("        Recommend Things To Enter Marwan👇")
+print("")
+print("            Pakistan Zindabad 👍🔥")
+print("")
+print("\033[1;32m ! :- 5 Winner  ko 100Rs 100Rs mile ge Aur Koch   Account")
+print("\033[1;33m ! :- Note All countries allowed")
+print("\033[1;35m 1 :- Subscribe Like And Comment")
+print("\033[1;36m 2 :- Join My Group ")
+print("\033[1;37m 3 :- Send Name Or Screen Shot Subscribe Ka")
+print("")
+print("\033[1;33m   [1] Subscribe Our Channel")
+Ahmad = input()
+if Ahmad in ["", " "]:
+    print (" [!] Please Select Correct Option")
+    exit()
+elif Ahmad in ["1", "01"]:
+    os.system('xdg-open https://www.facebook.com/sajid.thekedar')
+    time.sleep(1.0)
+    os.system('clear')
+    print(logo)
+print("[🔥]••••••••••••{AfriDii is Back}••••••••••••[🔥]")
+print("")
+print("        Recommend Things To Enter Marwan👇")
+print("")
+print("\033[1;35m 1 :- subscribe like and comment")
+print("\033[1;36m 2 :- FoLLow my Acc ")
+print("\033[1;37m 3 :- Send Name or Screen shot Subscribe ka")
+print("")
+print("\033[1;36m   [2] FoLLow my Acc")
+Ahmad = input()
+if Ahmad in [""]:
+    print (" [!] Please Select Correct Option")
+    exit()
+elif Ahmad in ["2", "02"]:
+	os.system('xdg-open https://www.facebook.com/profile.php?id=100057281774018')
+	time.sleep(1.0)
+	os.system('clear')
+	print(logo)
+print("[🔥]•••••••••••••••••{Y + K}•••••••••••••••••[🔥]")
+print("")
+print("        Recommend Things To Enter AfriDii👇")
+print("")
+print("\033[1;35m 1 :- subscribe like and comment")
+print("\033[1;36m 2 :- FoLLow my Acc ")
+print("\033[1;37m 3 :- Send Name or Screen shot Subscribe ka")
+print("")
+print("\033[1;32m   [?] Enter Your Name And Send Me")
+ah = input('\n\033[1;31m    Type Your Name :-\033[1;33m ')
+tks = 'Dear%20Sir,%20Please%20Add%20My%20Name%20In%20Marwan%20%20%20%20%20My%20%20Name%20%20:%20'+ah
+os.system('am start https://wa.me/+994400511189?text=' + tks)
+time.sleep(1.0)
+print('\n\033[1;32m   Congratulations Your Name Enter To AfriDii')
+print("")
+input("            \033[1;33m     Press Enter")
+os.system('clear')
+print(logo)
+
+notice()
